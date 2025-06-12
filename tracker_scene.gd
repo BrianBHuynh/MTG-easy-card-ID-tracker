f4a94282d1241fc0c668fb63568f8fe8b206fe55
Brian Huynh <brian123huy@gmail.com>
2025-06-12 00:58:59 -0400

    Slight scaling
    Ui Slightly scales more now

--- tracker_scene.gd
+++ tracker_scene.gd
@@ -9,11 +9,13 @@ var last_input: int = -1
 
 func _ready() -> void:
 	process_text()
-	while($VBoxContainer/Range/Min == null and Saves.save_loaded == false):
+	while($HBoxContainer/VBoxContainer/Range/Min == null and Saves.save_loaded == false):
 		await get_tree().process_frame
-	$VBoxContainer/SetID/SetID.text = Saves.get_or_return("Settings", "set_id", "")
-	$VBoxContainer/Range/Min.text = Saves.get_or_return("Settings", "min", "1")
-	$VBoxContainer/Range/Max.text = Saves.get_or_return("Settings", "max", "1")
+	$HBoxContainer/VBoxContainer/SetID/SetID.text = Saves.get_or_return("Settings", "set_id", "")
+	$HBoxContainer/VBoxContainer/Range/Min.text = Saves.get_or_return("Settings", "min", "1")
+	$HBoxContainer/VBoxContainer/Range/Max.text = Saves.get_or_return("Settings", "max", "1")
+	get_tree().root.size_changed.connect(display_changed_size)
+	display_changed_size()
 
 
 func _physics_process(_delta: float) -> void:
@@ -24,6 +26,10 @@ func _physics_process(_delta: float) -> void:
 		time_since_last_refresh = time_since_last_refresh + 1
 
 
+func display_changed_size() -> void:
+	$HBoxContainer/VBoxContainer.size = get_tree().root.size
+	$HBoxContainer.size = get_tree().root.size
+
 func process_text(_input: String = "") -> void:
 	var unacquired_list: String = "Unaquired: "
 	var unacquired_num: int = 0
@@ -31,12 +37,12 @@ func process_text(_input: String = "") -> void:
 	var duplicates_string: String = "duplicates: "
 	var out_of_range: String = "Out of Range: "
 	var play_sets: String = "Playsets: "
-	for i in int($VBoxContainer/Range/Max.text) - int($VBoxContainer/Range/Min.text) + 1:
-		if Saves.get_or_return($VBoxContainer/SetID/SetID.text, str(i + int($VBoxContainer/Range/Min.text)), 0) == 0:
-			unacquired_list = unacquired_list + " " + str(i + int($VBoxContainer/Range/Min.text))
+	for i in int($HBoxContainer/VBoxContainer/Range/Max.text) - int($HBoxContainer/VBoxContainer/Range/Min.text) + 1:
+		if Saves.get_or_return($HBoxContainer/VBoxContainer/SetID/SetID.text, str(i + int($HBoxContainer/VBoxContainer/Range/Min.text)), 0) == 0:
+			unacquired_list = unacquired_list + " " + str(i + int($HBoxContainer/VBoxContainer/Range/Min.text))
 			unacquired_num = unacquired_num + 1
-	if Saves.data is Dictionary and Saves.data.has($VBoxContainer/SetID/SetID.text) and Saves.data[$VBoxContainer/SetID/SetID.text] is Dictionary:
-		var keys: Array = Saves.data[$VBoxContainer/SetID/SetID.text].keys()
+	if Saves.data is Dictionary and Saves.data.has($HBoxContainer/VBoxContainer/SetID/SetID.text) and Saves.data[$HBoxContainer/VBoxContainer/SetID/SetID.text] is Dictionary:
+		var keys: Array = Saves.data[$HBoxContainer/VBoxContainer/SetID/SetID.text].keys()
 		var temp: Array = []
 		for key in keys:
 			temp.append(int(key))
@@ -45,60 +51,60 @@ func process_text(_input: String = "") -> void:
 		for key in temp:
 			keys.append(str(key))
 		for key in keys:
-			if Saves.data[$VBoxContainer/SetID/SetID.text][key] > 0:
-				duplicates[key] = Saves.data[$VBoxContainer/SetID/SetID.text][key]
-			if Saves.data[$VBoxContainer/SetID/SetID.text][key] >= 4:
+			if Saves.data[$HBoxContainer/VBoxContainer/SetID/SetID.text][key] > 0:
+				duplicates[key] = Saves.data[$HBoxContainer/VBoxContainer/SetID/SetID.text][key]
+			if Saves.data[$HBoxContainer/VBoxContainer/SetID/SetID.text][key] >= 4:
 				play_sets = play_sets + str(key) + " "
-			if int(key) > int($VBoxContainer/Range/Max.text) or int(key) < int($VBoxContainer/Range/Min.text):
-				out_of_range = out_of_range + " [" + str(key) + " , " + str(int(Saves.data[$VBoxContainer/SetID/SetID.text][key])) + "] "
+			if int(key) > int($HBoxContainer/VBoxContainer/Range/Max.text) or int(key) < int($HBoxContainer/VBoxContainer/Range/Min.text):
+				out_of_range = out_of_range + " [" + str(key) + " , " + str(int(Saves.data[$HBoxContainer/VBoxContainer/SetID/SetID.text][key])) + "] "
 	for key in duplicates:
 		if duplicates[key] > 1:
 			duplicates_string = duplicates_string + " [" + str(key) + " , " + str(int(duplicates[key] - 1)) + "] "
 	var final_string = ""
-	if $VBoxContainer/HBoxContainer/Unacquired.button_pressed:
+	if $HBoxContainer/VBoxContainer/HBoxContainer/Unacquired.button_pressed:
 		final_string = final_string + unacquired_list + "\nunacquired num = " + str(int(unacquired_num))
-	if $VBoxContainer/HBoxContainer/Duplicate.button_pressed:
+	if $HBoxContainer/VBoxContainer/HBoxContainer/Duplicate.button_pressed:
 		final_string = final_string + "\n\n" + duplicates_string
-	if $VBoxContainer/HBoxContainer/OutOfRange.button_pressed:
+	if $HBoxContainer/VBoxContainer/HBoxContainer/OutOfRange.button_pressed:
 		final_string = final_string + "\n\n" + out_of_range
-	if $VBoxContainer/HBoxContainer/PlaySets.button_pressed:
+	if $HBoxContainer/VBoxContainer/HBoxContainer/PlaySets.button_pressed:
 		final_string = final_string + "\n\n" + play_sets
-	if $VBoxContainer/HBoxContainer/AllCardsInSet.button_pressed:
+	if $HBoxContainer/VBoxContainer/HBoxContainer/AllCardsInSet.button_pressed:
 		var temp: Array = []
-		for card in Saves.data.get($VBoxContainer/SetID/SetID.text, ["No cards"]):
+		for card in Saves.data.get($HBoxContainer/VBoxContainer/SetID/SetID.text, ["No cards"]):
 			temp.append(int(card))
 		temp.sort()
 		var temp_2_electric_boogalo: Dictionary = {}
 		for card in temp:
-			temp_2_electric_boogalo.set(card, int(Saves.get_or_return($VBoxContainer/SetID/SetID.text, str(card), "0")))
+			temp_2_electric_boogalo.set(card, int(Saves.get_or_return($HBoxContainer/VBoxContainer/SetID/SetID.text, str(card), "0")))
 		var all_cards: String = "\n\n\n All cards: "
 		for key in temp_2_electric_boogalo:
 			all_cards = all_cards + " [" + str(key) + " , " + str(temp_2_electric_boogalo[key]) + "] "
 		final_string = final_string + all_cards
 		
 	final_string = final_string + "\n\n\nLast Input: " + str(last_input)
-	$VBoxContainer/RichTextLabel.text = final_string
+	$HBoxContainer/VBoxContainer/RichTextLabel.text = final_string
 
 
 func _on_card_submitted(new_text: String) -> void:
 	if new_text != "":
-		Saves.set_value($VBoxContainer/SetID/SetID.text, str(int(new_text)), int(Saves.get_or_return($VBoxContainer/SetID/SetID.text, new_text, 0) + 1))
-		if Saves.get_or_return($VBoxContainer/SetID/SetID.text, new_text, 1) == 1:
-			$VBoxContainer/ColorRect.color = Color.GREEN
+		Saves.set_value($HBoxContainer/VBoxContainer/SetID/SetID.text, str(int(new_text)), int(Saves.get_or_return($HBoxContainer/VBoxContainer/SetID/SetID.text, new_text, 0) + 1))
+		if Saves.get_or_return($HBoxContainer/VBoxContainer/SetID/SetID.text, new_text, 1) == 1:
+			$HBoxContainer/VBoxContainer/ColorRect.color = Color.GREEN
 		else:
-			$VBoxContainer/ColorRect.color = Color.RED
+			$HBoxContainer/VBoxContainer/ColorRect.color = Color.RED
 	last_input = int(new_text)
 	process_text()
-	$VBoxContainer/Add.text = ""
+	$HBoxContainer/VBoxContainer/Add.text = ""
 	Saves.save_game()
 
 
 func _on_card_removed(new_text: String) -> void:
 	if new_text != "":
-		Saves.set_value($VBoxContainer/SetID/SetID.text, str(int(new_text)), int(Saves.get_or_return($VBoxContainer/SetID/SetID.text, new_text, 0) -1))
+		Saves.set_value($HBoxContainer/VBoxContainer/SetID/SetID.text, str(int(new_text)), int(Saves.get_or_return($HBoxContainer/VBoxContainer/SetID/SetID.text, new_text, 0) -1))
 	last_input = int(new_text)
 	process_text()
-	$VBoxContainer/Remove.text = ""
+	$HBoxContainer/VBoxContainer/Remove.text = ""
 	Saves.save_game()
 
 func _on_copy_all_button_pressed() -> void:
@@ -106,16 +112,16 @@ func _on_copy_all_button_pressed() -> void:
 
 
 func _on_copy_set_pressed() -> void:
-		if Saves.data is Dictionary and Saves.data.has($VBoxContainer/SetID/SetID.text) and Saves.data[$VBoxContainer/SetID/SetID.text] is Dictionary:
-			DisplayServer.clipboard_set(str(Saves.data[$VBoxContainer/SetID/SetID.text]))
+		if Saves.data is Dictionary and Saves.data.has($HBoxContainer/VBoxContainer/SetID/SetID.text) and Saves.data[$HBoxContainer/VBoxContainer/SetID/SetID.text] is Dictionary:
+			DisplayServer.clipboard_set(str(Saves.data[$HBoxContainer/VBoxContainer/SetID/SetID.text]))
 
 
 func set_new_card_display(enabled: bool) -> void:
-	$VBoxContainer/ColorRect.visible = enabled
+	$HBoxContainer/VBoxContainer/ColorRect.visible = enabled
 
 
 func set_settings(_text: String) -> void:
-	Saves.set_value("Settings", "set_id", $VBoxContainer/SetID/SetID.text)
-	Saves.set_value("Settings", "min", $VBoxContainer/Range/Min.text)
-	Saves.set_value("Settings", "max", $VBoxContainer/Range/Max.text)
+	Saves.set_value("Settings", "set_id", $HBoxContainer/VBoxContainer/SetID/SetID.text)
+	Saves.set_value("Settings", "min", $HBoxContainer/VBoxContainer/Range/Min.text)
+	Saves.set_value("Settings", "max", $HBoxContainer/VBoxContainer/Range/Max.text)
 	Saves.save_game()
